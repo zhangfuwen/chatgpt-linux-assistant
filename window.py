9ddf8b817e1e30ce01aaf78d7d47d026283d3000
zhangfuwen <zhangfuwen@foxmail.com>
2023-09-15 16:24:08 +0800

    add avatar

--- window.py
+++ window.py
@@ -1,11 +1,29 @@
+import os.path
 import tkinter as tk
+import markdown
+from tkhtmlview import HTMLScrolledText
+
+message_template="""
+<div class="msg" style="display: block; width:80%">
+    <div class="user" style="display:inline; width:100%; height: 10px">
+        <div class="avatar" style="height: 28px; display:inline;">
+            <img src="{avatar}" style="width:28px; height: 28px" />
+            <span style="height: 28px">{name}:{html}</span>
+        </div>
+    </div>
+    <div class="msg_content" style="width:80%; ">
+    </div>
+</div>
+"""
 
 class ChatWindow:
     def __init__(self, callback=None):
         self.root = tk.Tk()
         self.root.title("Chat Window")
 
-        self.chat_box = tk.Text(self.root, height=10, width=50)
+        # self.chat_box = tk.Text(self.root, height=10, width=50)
+        self.chat_box = HTMLScrolledText(self.root, height=10, width=50)
+        self.chat_content =""
         self.chat_box.pack()
 
         self.input_area = tk.Text(self.root, height=3, width=50)
@@ -24,12 +42,18 @@ class ChatWindow:
         self.send_message()
 
     def other_message(self, text):
-        self.chat_box.insert(tk.END, f"{text}\n")
+        html = markdown.markdown(text)
+        self.chat_box.insert(tk.END, f"{html}\n")
 
     def send_message(self):
         message = self.input_area.get("1.0", tk.END).strip()
         if message:
-            self.chat_box.insert(tk.END, f"You: {message}\n")
+            html = markdown.markdown(message)
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+
+            msg=message_template.format(avatar=script_dir+"/avatar.png", name="Human", html=html)
+            self.chat_content = self.chat_content + msg
+            self.chat_box.set_html(self.chat_content)
             self.input_area.delete("1.0", tk.END)
             if self.callback:
                 self.callback(message)
